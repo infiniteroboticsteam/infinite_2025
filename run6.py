b38ac1b0f75bd8bfde3d6e2040d499a63b4612fc
ho2o6 <ethan.lei.wang@gmail.com>
2026-01-23 20:03:53 -0500

    final update run 6

--- run6.py
+++ run6.py
@@ -12,7 +12,7 @@ from pybricks.parameters import Axis, Direction, Port
 async def run6():
     await HUB.speaker.beep()
     DRIVE_BASE.use_gyro(True) 
-    await DRIVE_BASE.turn(25)
+    await DRIVE_BASE.turn(20)
     await DRIVE_BASE.straight(400)
     wait(500)
     await DRIVE_BASE.straight(-300)
